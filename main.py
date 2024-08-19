e70582ff6075867d23991fe39f3e973b7f69ec3d
Bodam Kim <88372309+qhdamm@users.noreply.github.com>
2024-08-19 23:50:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,40 +19,6 @@ load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-# 섹션 제목 리스트 정의
-section_titles = [
-    "GENERAL", "COMPETITIONS", "COMPETITION FORMAT", "STATEMENT OF DIVES",
-    "COMPETITION PROCEDURE", "DUTIES OF THE REFEREE AND ASSISTANT REFEREES",
-    "DUTIES OF THE SECRETARIAT", "JUDGING", 
-    "REFEREEING AND JUDGING SYNCHRONISED DIVING",
-    "SUMMARY OF THE PENALTIES", 
-    "DIVING AT THE WORLD AQUATICS CHAMPIONSHIPS AND OLYMPIC GAMES",
-    "AGE GROUP RULES - DIVING", "DIVING FACILITIES AND EQUIPMENT",
-    "MEDICAL AND SAFETY SPECIFIC REQUIREMENT FOR DIVING"
-]
-
-pdf_path = './rag/data/Competition-Regulations.pdf'
-sections = split_pdf_by_section_titles(pdf_path, section_titles)
-
-# 텍스트 임베딩 생성
-model = SentenceTransformer('all-MiniLM-L6-v2') # 임베딩 모델 로드
-
-# PDF에서 추출한 텍스트 임베딩 생성
-sections = split_pdf_by_section_titles(pdf_path, section_titles)
-embeddings = model.encode(sections)
-
-# 2. 고정 데이터: 컬렉션 생성 또는 가져오기
-collection = client.get_or_create_collection("pdf_collection") # 고정 데이터 
-
-# 임베딩된 텍스트를 컬렉션에 추가
-for i, section in enumerate(sections):
-    collection.add(
-        documents=[section],
-        embeddings=[embeddings[i].tolist()],  # 임베딩을 리스트로 변환
-        ids=[f"pdf_section_{i}"]
-    )
-    
-
 
 new_parser = argparse.ArgumentParser(description="Extract dive data to be used for scoring.")
 new_parser.add_argument("video_path", type=str, help="Path to dive video (mp4 format).")
@@ -69,14 +35,18 @@ with open(save_path_html, 'r') as file:
     html_content = file.read()
 
 # Start RAG
+db_handler = ChromaDBHandler()
+## pdf file -> db
+pdf_path = './rag/data/Competition-Regulations.pdf'
+store_pdf(pdf_path, db_handler)
+## html file -> db
 text_content = extract_text_from_html(html_content)
 embedding = generate_embeddings(text_content)
-db_handler = ChromaDBHandler()
-db_handler.store_embedding(document_id, text_content, embedding)
+db_handler.store_embedding(html_id, text_content, embedding)
 print("Document stored successfully.")
 # Example RAG Query
 response = generate_response_with_rag("Explain the performance analysis.", db_handler)
 print(f"Response: {response}")
 if meta_program_args.html_info_delete:
-    db_handler.delete_document(document_id)
+    db_handler.delete_document(html_id)
     print("Document deleted successfully.")

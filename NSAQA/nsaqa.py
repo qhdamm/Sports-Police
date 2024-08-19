e9b2f8f1d6deafcec4e452cbf215dc5ba478d0d3
1nhye <inhyeyang@yonsei.ac.kr>
2024-08-19 19:20:29 +0000

    add ocr

--- NSAQA/nsaqa.py
+++ NSAQA/nsaqa.py
@@ -10,13 +10,13 @@ from models.detectron2.detectors import get_platform_detector, get_diver_detecto
 from models.pose_estimator.pose_estimator_model_setup import get_pose_estimation, get_pose_model
 from rule_based_programs.scoring_functions import *
 from score_report_generation.generate_report_functions import *
-from rule_based_programs.aqa_metaProgram import aqa_metaprogram, abstractSymbols, extract_frames
+from rule_based_programs.aqa_metaProgram import add_difficulty, aqa_metaprogram, abstractSymbols, extract_frames, extract_frames_ocr
 import argparse
 from bs4 import BeautifulSoup
 import openai
 
 
-def main(video_path, difficulty):
+def main(video_path):
     platform_detector = get_platform_detector()
     splash_detector = get_splash_detector()
     diver_detector = get_diver_detector()
@@ -26,10 +26,12 @@ def main(video_path, difficulty):
     dive_data = {}
 
     frames = extract_frames(video_path)
+    ocr_frames = extract_frames_ocr(video_path)
     dive_data = abstractSymbols(frames, platform_detector=platform_detector, splash_detector=splash_detector, diver_detector=diver_detector, pose_model=pose_model)
+    dive_data = add_difficulty(ocr_frames, dive_data)   # difficulty 추가 부분
     dive_data = aqa_metaprogram(frames, dive_data, platform_detector=platform_detector, splash_detector=splash_detector, diver_detector=diver_detector, pose_model=pose_model)
-    # difficulty 추가 부분
-    intermediate_scores = get_all_report_scores(dive_data, difficulty)
+    
+    intermediate_scores = get_all_report_scores(dive_data)
     html, html_id = generate_report_from_frames(template_path, intermediate_scores, frames)
     summary_html, summary_html_id = generate_report_from_frames(summary_template_path, intermediate_scores, frames)
 

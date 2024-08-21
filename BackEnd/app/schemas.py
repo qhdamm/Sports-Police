5534701212201699073b8608d6b62d994047bd2c
kjinh <69679512+kjinh@users.noreply.github.com>
2024-08-21 10:04:21 +0900

    Update schemas.py

--- BackEnd/app/schemas.py
+++ BackEnd/app/schemas.py
@@ -9,7 +9,16 @@ class YouTubeLink(BaseModel):
 class AnalysisResult(BaseModel):
     gt_score: float
     report: bytes # html report of model output
+    document_id: str  # 문서 ID
+    video_name: str
 
+class QAInput(BaseModel):
+    user_input: str
+    document_id: str
+    video_name: str
 
 class QAResults(BaseModel):
     answer: str
+
+class goHome(BaseModel):
+    document_id: str
